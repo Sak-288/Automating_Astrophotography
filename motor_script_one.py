92e9c26bce7098b74ee4ad0952edb612793ce650
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-04 21:45:48 +0100

    fixed flawed wiring. testing things out

--- motor_script_one.py
+++ motor_script_one.py
@@ -19,23 +19,20 @@ GPIO.setup(MS3, GPIO.OUT)
 
 GPIO.output(DIR, GPIO.HIGH)
 GPIO.output(STEP, GPIO.LOW) 
-GPIO.output(MS1, GPIO.LOW)
-GPIO.output(MS2, GPIO.LOW) 
-GPIO.output(MS3, GPIO.LOW)
+GPIO.output(MS1, GPIO.HIGH)
+GPIO.output(MS2, GPIO.HIGH) 
+GPIO.output(MS3, GPIO.HIGH)
 
 target = 360 * 10
 
 # Since MS1-2 && 3 are set to HIGH, we are dealing in 1/16th steps
 
-nMicro = 1
+nMicro = 16
 nSteps = round(target / 1.8 * nMicro)
-print(nSteps)
-DELAY = 0.02
+DELAY = 1/nSteps * 5
 
 for i in range(nSteps):
     GPIO.output(STEP, GPIO.HIGH)
     time.sleep(DELAY)
     GPIO.output(STEP, GPIO.LOW)
     time.sleep(DELAY)
-
-GPIO.cleanup()
