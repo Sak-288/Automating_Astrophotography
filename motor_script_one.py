5c3fc9f77b8120a8f8599aeacf65ac587e83aa07
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-04 18:04:19 +0100

    I hate my life.

---
+++ motor_script_one.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import math
+import time
+
+DIR = 11
+STEP = 13
+MS1 = 15
+MS2 = 16
+MS3 = 18
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(MS1, GPIO.OUT)
+GPIO.setup(MS2, GPIO.OUT)
+GPIO.setup(MS3, GPIO.OUT)
+
+GPIO.output(DIR, GPIO.HIGH) 
+GPIO.output(STEP, GPIO.LOW) 
+GPIO.output(MS1, GPIO.HIGH) 
+GPIO.output(MS2, GPIO.HIGH) 
+GPIO.output(MS3, GPIO.HIGH) 
+
+target = 90
+
+# Since MS1-2 && 3 are set to HIGH, we are dealing in 1/16th steps
+
+nMicro = 16
+nSteps = target / 1.8 * nMicro
+
+for i in range(nSteps):
+    GPIO.output(STEP, GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(STEP, GPIO.LOW)
+    time.sleep(0.001)
+
+GPIO.cleanup()
