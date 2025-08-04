df4240cd97595ac13938cffdc82c79ccb89bf309
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-04 22:00:39 +0100

    made some minor mistakes + one major (didn'th ground the enable pin on the A4988 driver)

--- motor_script_one.py
+++ motor_script_one.py
@@ -3,6 +3,7 @@ import time
 
 print('hello world I am working !')
 
+EN = 12
 DIR = 20
 STEP = 21
 MS1 = 23
@@ -10,29 +11,29 @@ MS2 = 24
 MS3 = 25
 
 GPIO.setmode(GPIO.BCM)
-
+GPIO.setup(EN, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 GPIO.setup(MS1, GPIO.OUT)
 GPIO.setup(MS2, GPIO.OUT)
 GPIO.setup(MS3, GPIO.OUT)
 
+GPIO.output(EN, GPIO.LOW)
 GPIO.output(DIR, GPIO.HIGH)
-GPIO.output(STEP, GPIO.HIGH) 
 GPIO.output(MS1, GPIO.HIGH)
-GPIO.output(MS2, GPIO.HIGH) 
+GPIO.output(MS2, GPIO.HIGH)
 GPIO.output(MS3, GPIO.HIGH)
 
 target = 180
-
-# Since MS1-2 && 3 are set to HIGH, we are dealing in 1/16th steps
-
 nMicro = 16
 nSteps = round(target / 1.8 * nMicro)
-DELAY = 1/nSteps * 5
+DELAY = 0.005  # You can reduce this gradually to increase speed
 
-for i in range(nSteps):
-    GPIO.output(STEP, GPIO.HIGH)
-    time.sleep(DELAY)
-    GPIO.output(STEP, GPIO.LOW)
-    time.sleep(DELAY)
+try:
+    for i in range(nSteps):
+        GPIO.output(STEP, GPIO.HIGH)
+        time.sleep(DELAY)
+        GPIO.output(STEP, GPIO.LOW)
+        time.sleep(DELAY)
+finally:
+    GPIO.cleanup()
