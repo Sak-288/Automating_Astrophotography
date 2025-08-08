0870bcc351c155b53f075b9a4630f32581affbe3
Amine Sakoute <aminesakoute288gmail.com>
2025-08-08 16:15:31 +0100

    Uh Idk

--- sensor_behaviour/mpu6050_main_function_test.py
+++ sensor_behaviour/mpu6050_main_function_test.py
@@ -1,3 +1,4 @@
+# Importing the related libraries
 from mpu6050 import mpu6050
 import time
 mpu = mpu6050(0x68)
@@ -6,16 +7,10 @@ while True:
     print("Temp : "+str(mpu.get_temp()))
     print()
 
-    accel_data = mpu.get_accel_data()
-    print("Acc X : "+str(accel_data['x']))
-    print("Acc Y : "+str(accel_data['y']))
-    print("Acc Z : "+str(accel_data['z']))
-    print()
-
+    # Basically just reading the data of the 3 axis : pitch, yaw, roll
     gyro_data = mpu.get_gyro_data()
-    print("Gyro X : "+str(gyro_data['x']))
-    print("Gyro Y : "+str(gyro_data['y']))
-    print("Gyro Z : "+str(gyro_data['z']))
+    print("Gyro X : "+str(gyro_data['x'])) # yaw
+    print("Gyro Y : "+str(gyro_data['y'])) # pitch
+    print("Gyro Z : "+str(gyro_data['z'])) # roll
     print()
     print("-------------------------------")
-    time.sleep(1)
