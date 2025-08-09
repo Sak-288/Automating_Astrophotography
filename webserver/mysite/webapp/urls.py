bbef32ff6011ea80ba08e8c519345559c80314e6
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-09 22:19:05 +0100

    HJAJAJ

--- webserver/mysite/webapp/urls.py
+++ webserver/mysite/webapp/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),        # for root URL (e.g. /)
     path('home/', views.home, name='home'),   # for /home URL
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
