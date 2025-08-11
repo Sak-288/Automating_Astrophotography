96f6d6193ed34a8a95e76e0be1a0ceb430085daa
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-11 20:53:56 +0100

    Just testing something

--- webserver/mysite/astronomers/views.py
+++ webserver/mysite/astronomers/views.py
@@ -13,6 +13,6 @@ def login_astronomer(request):
             return redirect('home')
         else:
             messages.error(request, 'There was an error logging in, TRY AGAIN!')
-            return redirect('login')
+            # return redirect('login')
     else:
         return render(request, 'registration/login.html', {})
