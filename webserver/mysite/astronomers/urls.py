96e66c8f2f6569d49b638d21d82068b61b1ed7be
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-11 18:43:14 +0100

    just follow the instructions !

--- webserver/mysite/astronomers/urls.py
+++ webserver/mysite/astronomers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login_astronomer', views.login_astronomer, name="login"),    
+    path('login_astronomer', views.login_astronomer, name="login"),
+    
 ]
