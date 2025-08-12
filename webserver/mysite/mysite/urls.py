df8acf90644cdc459a1f95fde9c957809156352e
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-12 22:55:04 +0100

    How did I not see that before ?

--- webserver/mysite/mysite/urls.py
+++ webserver/mysite/mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('astronomers/', include('astronomers.urls')),
