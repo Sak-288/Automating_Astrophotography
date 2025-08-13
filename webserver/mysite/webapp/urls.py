3979b4fc2daed7cb8ad643883d591090cb65a8ab
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-13 17:46:45 +0100

    HAD FORGOTTEN TO ADD THE HMTL FILE TO URLS.PY

--- webserver/mysite/webapp/urls.py
+++ webserver/mysite/webapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.home, name='home'),        # for root URL (e.g. /)
     path('home/', views.home, name='home'),   # for /home URL
     path('contact.html', views.contact, name='contact'),   # for /home URL
+    path('contact_successful.html', views.contact, name='contact_successful'),   # for /home URL
+    path('test_for_animation.html', views.contact, name='test_for_animation'),   # for /home URL
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
