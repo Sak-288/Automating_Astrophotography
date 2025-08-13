0ccae6bf3993104d26fab3f3d733b0ee75310b9c
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-13 12:07:58 +0100

    ????

--- webserver/mysite/astronomers/urls.py
+++ webserver/mysite/astronomers/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login_astronomer', views.login_astronomer, name="login"),  
     path('logout_astronomer', views.logout_astronomer, name="logout"), 
+    path('register_astronomer', views.register_astronomer, name="register_astronomer"), 
 ]
--- webserver/mysite/astronomers/views.py
+++ webserver/mysite/astronomers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages import *
+from django.contrib.auth.forms import UserCreationForm
 
 def login_astronomer(request):
     if request.method == "POST":
@@ -19,3 +20,6 @@ def login_astronomer(request):
 def logout_astronomer(request):
     logout(request)
     return redirect('home')
+
+def register_astronomer(request):
+    return render(request, 'registration/register_astronomer.html', {})
