9f7a9359cf6bde5fd2463e5a684a028d8145ebe4
Amine Sakoute <aminesakoute288@gmail.com>
2025-08-13 13:10:05 +0100

    forgot to import forms.py

--- webserver/mysite/astronomers/views.py
+++ webserver/mysite/astronomers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages import *
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterAstronomerForm
 
 def login_astronomer(request):
     if request.method == "POST":
